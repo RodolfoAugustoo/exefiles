3f350c49b873d8e0de8a16cf3bd221b01af59d32
Rodolfo Augusto <r.augusto1892@gmail.com>
2020-01-19 09:33:26 -0300

    Exercicio de arquivos.

---
+++ ExeFiles/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System;
+
+namespace ExeFiles.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Total()
+        {
+            double total = Price * Quantity;
+            return (" $" + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ExeFiles/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using ExeFiles.Entities;
+
+namespace ExeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the full file path: ");
+            string sourceFilePath = Console.ReadLine();
+            
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                if (!Directory.Exists(targetFolderPath))
+                {
+                    Directory.CreateDirectory(targetFolderPath);
+                }
+                
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string l in lines)
+                    {
+                        string[] fields = l.Split(",");
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+                        sw.WriteLine(name + product.Total());
+                        //Console.WriteLine(name + product.Total());
+                    }   
+                }
+
+                using(StreamReader sr = File.OpenText(targetFilePath))
+                {
+                    while (!sr.EndOfStream)
+                    {                        
+                        Console.WriteLine("Product: " + sr.ReadLine());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("\r\nAn error occured: " + e.Message);
+            }
+        }
+    }
+}
